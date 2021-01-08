ef3571fd8c27163d718b02a75bf1f5c27f7e31ce
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-01-08 16:30:36 -0300

    feat: make conversion optional in persistence layer

--- commons-data-impl/src/test/java/com/appjars/saturn/dao/PersonCrudDaoImpl.java
+++ commons-data-impl/src/test/java/com/appjars/saturn/dao/PersonCrudDaoImpl.java
@@ -26,7 +26,7 @@ import com.appjars.saturn.dao.jpa.JpaDaoSupport;
 import com.appjars.saturn.model.impl.Person;
 
 public class PersonCrudDaoImpl
-		implements JpaDaoSupport<Person, Person, Integer>, EntityConversionCrudDaoMixin<Person, Integer> {
+		implements JpaDaoSupport<Person, Person, Integer> {
 
 	EntityManagerFactory entityManagerFactory;
 
