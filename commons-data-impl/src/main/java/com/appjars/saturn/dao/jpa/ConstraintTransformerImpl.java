f3102e79d7ce867002522b5e83ee56fb01b8dd40
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-02-17 14:15:06 -0300

    feat: implement JPA ConstraintTransformer

---
+++ commons-data-impl/src/main/java/com/appjars/saturn/dao/jpa/ConstraintTransformerImpl.java
@@ -0,0 +1,80 @@
+package com.appjars.saturn.dao.jpa;
+
+import java.util.Objects;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import com.appjars.saturn.model.ConstraintTransformer;
+import com.appjars.saturn.model.constraints.AttributeBetweenConstraint;
+import com.appjars.saturn.model.constraints.AttributeConstraint;
+import com.appjars.saturn.model.constraints.AttributeInConstraint;
+import com.appjars.saturn.model.constraints.AttributeLikeConstraint;
+import com.appjars.saturn.model.constraints.AttributeRelationalConstraint;
+import com.appjars.saturn.model.constraints.NegatedConstraint;
+import com.appjars.saturn.model.constraints.RelationalConstraint;
+
+public class ConstraintTransformerImpl extends ConstraintTransformer<Predicate> {
+
+	private final CriteriaBuilder criteriaBuilder;
+	private Root<?> root;
+	
+	public ConstraintTransformerImpl(EntityManager em, Root<?> root) {
+		this.criteriaBuilder = em.getCriteriaBuilder();
+		this.root = Objects.requireNonNull(root);
+	}
+
+	private Expression<?> getExpression(AttributeConstraint c) {
+		return root.get(c.getAttribute());
+	}
+	
+	@SuppressWarnings("unchecked")
+	private <T> Expression<T> getExpression(AttributeConstraint c, Class<T> type) {
+		Expression<?> expression = root.get(c.getAttribute());
+		expression.getJavaType().asSubclass(type);
+		return (Expression<T>) expression;
+	}
+		
+	@Override
+	protected Predicate transformNegatedConstraint(NegatedConstraint c) {		
+		return criteriaBuilder.not(transform(c.getConstraint()));
+	}
+
+	@Override
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	protected Predicate transformRelationalConstraint(AttributeRelationalConstraint c) {
+		c.getAttribute();
+		Expression<Comparable> x = getExpression(c, Comparable.class);
+		Comparable y = c.getValue();
+		
+		switch (c.getOperator()) {
+			case RelationalConstraint.EQ: return criteriaBuilder.equal(x, y);
+			case RelationalConstraint.NE: return criteriaBuilder.notEqual(x, y);
+			case RelationalConstraint.LE: return criteriaBuilder.lessThanOrEqualTo(x, y);
+			case RelationalConstraint.LT: return criteriaBuilder.lessThan(x, y);
+			case RelationalConstraint.GE: return criteriaBuilder.greaterThan(x, y);
+			case RelationalConstraint.GT: return criteriaBuilder.greaterThanOrEqualTo(x, y);
+			default: return null;
+		}
+	}
+
+	@Override
+	protected Predicate transformLikeConstraint(AttributeLikeConstraint c) {
+		return criteriaBuilder.like(getExpression(c, String.class), c.getPattern());
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	protected Predicate transformBetweenConstraint(AttributeBetweenConstraint c) {
+		return criteriaBuilder.between(getExpression(c, Comparable.class), (Comparable)c.getLower(), (Comparable)c.getUpper());
+	}
+
+	@Override
+	protected Predicate transformInConstraint(AttributeInConstraint c) {
+		return getExpression(c).in(c.getValues());
+	}
+
+}
