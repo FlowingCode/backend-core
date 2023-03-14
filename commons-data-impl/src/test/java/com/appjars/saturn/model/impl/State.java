7c93d542e4099d22f99217a1aaf03aa3f333ac83
Martín López <mlopez@flowingcode.com>
2023-03-14 18:12:57 -0300

    test: use IDENTITY generation type instead of AUTO

--- commons-data-impl/src/test/java/com/appjars/saturn/model/impl/State.java
+++ commons-data-impl/src/test/java/com/appjars/saturn/model/impl/State.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 public class State extends BaseEntity<Integer> {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Column
